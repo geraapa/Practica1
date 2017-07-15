39d401c101faa2776cfc1b0c5c9d7a3f39832941
geraapa <geraapa@gmail.com>
2017-07-15 11:10:06 -0500

    calculadoraV2

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -11,51 +11,63 @@ namespace HolaMundo
         static void Main(string[] args)
         {
             int operacion;
+            string repite;
             double valor1, valor2, resultado;
             bool entra;
-            entra = true;
-            operacion = 0;
-            valor1 = 0.0;
-            valor2 = 0.0;
-            resultado = 0.0;
-            //Calculadora 
-            Console.WriteLine("CALCULADORA");
-
-            Console.WriteLine("1.- Suma");
-            Console.WriteLine("2.- Resta");
-            Console.WriteLine("3.- Multiplicación");
-            Console.WriteLine("4.- División");
-            operacion = Int32.Parse(Console.ReadLine());
-            Console.Clear();
-            switch (operacion)
+            do
             {
-                case 1: Console.WriteLine("Suma");
-                        break;
-                case 2: Console.WriteLine("Resta");
-                        break;
-                case 3: Console.WriteLine("Multiplicación");
-                        break;
-                case 4: Console.WriteLine("División");
-                        break;
-                default: Console.WriteLine("Opción no valida");
-                         entra = false;
-                         break;
-            }
-            if (entra)
-            {
-                Console.WriteLine("Ingresa el valor 1:");
-                valor1 = Double.Parse(Console.ReadLine());
-                Console.WriteLine("Ingresa el valor 2:");
-                valor2 = Double.Parse(Console.ReadLine());
+                Console.Clear();
+                entra = true;
+                operacion = 0;
+                valor1 = 0.0;
+                valor2 = 0.0;
+                resultado = 0.0;
+                //Calculadora 
+                Console.WriteLine("CALCULADORA");
+                Console.WriteLine("1.- Suma");
+                Console.WriteLine("2.- Resta");
+                Console.WriteLine("3.- Multiplicación");
+                Console.WriteLine("4.- División");
+                operacion = Int32.Parse(Console.ReadLine());
+                Console.Clear();
                 switch (operacion)
                 {
-                    case 1: resultado = valor1 + valor2;
+                    case 1:
+                        Console.WriteLine("Suma");
+                        break;
+                    case 2:
+                        Console.WriteLine("Resta");
+                        break;
+                    case 3:
+                        Console.WriteLine("Multiplicación");
+                        break;
+                    case 4:
+                        Console.WriteLine("División");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no valida");
+                        entra = false;
+                        break;
+                }
+                if (entra)
+                {
+                    Console.WriteLine("Ingresa el valor 1:");
+                    valor1 = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el valor 2:");
+                    valor2 = Double.Parse(Console.ReadLine());
+                    switch (operacion)
+                    {
+                        case 1:
+                            resultado = valor1 + valor2;
                             break;
-                    case 2: resultado = valor1 - valor2;
+                        case 2:
+                            resultado = valor1 - valor2;
                             break;
-                    case 3: resultado = valor1 * valor2;
+                        case 3:
+                            resultado = valor1 * valor2;
                             break;
-                    case 4: if(valor2 == 0)
+                        case 4:
+                            if (valor2 == 0)
                             {
                                 Console.WriteLine("No se puede dividir entre 0.");
                             }
@@ -64,12 +76,13 @@ namespace HolaMundo
                                 resultado = valor1 / valor2;
                             }
                             break;
+                    }
+                    Console.WriteLine("El resultado es {0}", resultado);
                 }
-                Console.WriteLine("El resultado es {0}", resultado);
-                
+                Console.WriteLine("Quieres repetir? s/n");
+                repite = Console.ReadLine();
             }
-            
-            Console.ReadLine();
+            while (repite != "n" && repite != "N");
         }
     }
 }
